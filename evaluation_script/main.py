450d05d73f48b187f2b8b7c8bcc5022f938c822e
Bob Serling <62090961+sunlibo2390@users.noreply.github.com>
2024-03-13 14:50:41 +0800

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -3,8 +3,9 @@ import random
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
     print("Starting Evaluation.....")
+    print("Submission related metadata:")
     """
-    Evaluates the submission for a particular challenge phase and returns score
+    Evaluates the submission for a particular challenge phase adn returns score
     Arguments:
 
         `test_annotations_file`: Path to test_annotation_file on the server
@@ -19,63 +20,58 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         Example: A sample submission metadata can be accessed like this:
         >>> print(kwargs['submission_metadata'])
         {
-            'status': u'running',
-            'when_made_public': None,
-            'participant_team': 5,
-            'input_file': 'https://abc.xyz/path/to/submission/file.json',
-            'execution_time': u'123',
-            'publication_url': u'ABC',
-            'challenge_phase': 1,
-            'created_by': u'ABC',
-            'stdout_file': 'https://abc.xyz/path/to/stdout/file.json',
-            'method_name': u'Test',
-            'stderr_file': 'https://abc.xyz/path/to/stderr/file.json',
-            'participant_team_name': u'Test Team',
-            'project_url': u'http://foo.bar',
-            'method_description': u'ABC',
-            'is_public': False,
-            'submission_result_file': 'https://abc.xyz/path/result/file.json',
-            'id': 123,
-            'submitted_at': u'2017-03-20T19:22:03.880652Z'
+            "status": u"running",
+            "when_made_public": None,
+            "participant_team": 5,
+            "input_file": "https://abc.xyz/path/to/submission/file.json",
+            "execution_time": u"123",
+            "publication_url": u"ABC",
+            "challenge_phase": 1,
+            "created_by": u"ABC",
+            "stdout_file": "https://abc.xyz/path/to/stdout/file.json",
+            "method_name": u"Test",
+            "stderr_file": "https://abc.xyz/path/to/stderr/file.json",
+            "participant_team_name": u"Test Team",
+            "project_url": u"http://foo.bar",
+            "method_description": u"ABC",
+            "is_public": False,
+            "submission_result_file": "https://abc.xyz/path/result/file.json",
+            "id": 123,
+            "submitted_at": u"2017-03-20T19:22:03.880652Z",
         }
     """
+    print(kwargs["submission_metadata"])
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
+    if phase_codename == "product_csum":
+        print("Evaluating for Product-Csum Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "product_csum": {
+                    "BLEU": random.randint(0, 99),
+                    "ROUGE": random.randint(0, 99),
+                    "METEOR": random.randint(0, 99),
+                    "Compre.": random.randint(0, 99),
+                    "Relev.": random.randint(0, 99),
                 }
             }
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
+        print("Completed evaluation for Product-Csum Phase")
+    elif phase_codename == "product_csum_cross":
+        print("Evaluating for Product-Csum-Cross Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "product_csum_cross": {
+                    "BLEU": random.randint(0, 99),
+                    "ROUGE": random.randint(0, 99),
+                    "METEOR": random.randint(0, 99),
+                    "Compre.": random.randint(0, 99),
+                    "Relev.": random.randint(0, 99),
                 }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
+            }
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
+        print("Completed evaluation for Product-Csum-Cross Phase")
     return output
